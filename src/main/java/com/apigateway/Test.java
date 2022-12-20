48a9f2c24bb26e231d1289fea9624ca5382b4caf
Dilip <singhdilip.kr2015@gmail.com>
2022-12-21 00:47:28 +0530

    commit api gatway

---
+++ src/main/java/com/apigateway/Test.java
@@ -0,0 +1,12 @@
+package com.apigateway;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
+
+emp
+id,dept_id,sal
+        select id,dept_id sal from emp e1 selfjoin emp e2 where l1.dept_id=l2.dept_id;
+
